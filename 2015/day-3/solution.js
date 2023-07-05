1a2ce4d6fb928b2792f0506504e1a92aea3546b3
Utsab Bera <nCoder24@users.noreply.github.com>
2023-07-05 11:29:54 +0530

    add tests and part-2 solution

--- 2015/day-3/solution.js
+++ 2015/day-3/solution.js
@@ -2,32 +2,57 @@ const directions = {
   ">": [1, 0],
   "<": [-1, 0],
   "^": [0, 1],
-  "v": [0, -1],
-}
+  v: [0, -1],
+};
 
 const nextHouse = (house, direction) => {
   return [
     house[0] + directions[direction][0],
     house[1] + directions[direction][1],
   ];
-}
+};
 
-const countHouses = (instructions) => {
+const isEven = (number) => {
+  return number % 2 === 0;
+};
+
+const countVisitedHouses = (instructions) => {
   const housesVisited = new Set(["1,1"]);
-  let house = [1, 1];
+  let santaLocation = [1, 1];
 
   Array.from(instructions).forEach((direction) => {
-    house = nextHouse(house, direction);
-    housesVisited.add(house.join(","));
+    santaLocation = nextHouse(santaLocation, direction);
+    housesVisited.add(santaLocation.join(","));
   });
 
   return housesVisited.size;
-}
+};
 // TimeStamp: 32m
 
-const main  = () => {
+// Part - 2
+const countVisitedHousesWithRobo = (instructions) => {
+  const housesVisited = new Set(["1,1"]);
+
+  let houseToMoveFrom = [1, 1];
+  let nextHouseToMoveFrom = [1, 1];
+
+  Array.from(instructions).forEach((direction) => {
+    const house = nextHouse(houseToMoveFrom, direction);
+    housesVisited.add(house.join(","));
+
+    houseToMoveFrom = nextHouseToMoveFrom;
+    nextHouseToMoveFrom = house;
+  });
+
+  return housesVisited.size;
+};
+
+const main = () => {
   const instructions = require("fs").readFileSync("input", "utf-8");
-  console.log(countHouses(instructions));
-}
+  // console.log(countVisitedHouses(instructions));
+  console.log(countVisitedHousesWithRobo(instructions));
+};
 
 main();
+
+module.exports = { countVisitedHouses, countVisitedHousesWithRobo };
---
+++ 2015/day-3/test.js
@@ -0,0 +1,42 @@
+const { describe, it } = require("node:test");
+const { countVisitedHouses, countVisitedHousesWithRobo } = require("./solution");
+const assert = require("assert");
+
+describe("countVisitedHouses", () => {
+  it("should visit 1 house(starting house) with 0 instructions", () => {
+    const instructions = "";
+    assert.strictEqual(countVisitedHouses(instructions), 1);
+  });
+
+  it("should visit 2 houses for any one instruction", () => {
+    const instructions = "v";
+    assert.strictEqual(countVisitedHouses(instructions), 2);
+  });
+
+  it("should count houses for more than one instructions", () => {
+    const instructions = "^>v<";
+    assert.strictEqual(countVisitedHouses(instructions), 4);
+  });
+
+  it("should count a house only once", () => {
+    const instructions = "v^v^";
+    assert.strictEqual(countVisitedHouses(instructions), 2);
+  });
+});
+
+describe("countVisitedHousesWithRobo", () => {
+  it("should visit 1 house(starting house) with 0 instructions", () => {
+    const instructions = "";
+    assert.strictEqual(countVisitedHousesWithRobo(instructions), 1);
+  });
+
+  it("should alter the instructions", () => {
+    const instructions = "^v";
+    assert.strictEqual(countVisitedHousesWithRobo(instructions), 3);
+  });
+
+  it("should count unique houses visited by both robo-santa and santa", () => {
+    const instructions = "^v^v^v^v^v";
+    assert.strictEqual(countVisitedHousesWithRobo(instructions), 11);
+  });
+});
