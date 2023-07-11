fc21c04d024b5af0ba7eb2d3760e2f947651ddfd
Utsab Bera <nCoder24@users.noreply.github.com>
2023-07-11 10:12:09 +0530

    implement countLitLights method

--- 2015/day-6/src/light-grid.js
+++ 2015/day-6/src/light-grid.js
@@ -1,7 +1,20 @@
+const makeGrid = (height, width, filler) =>
+  new Array(height).fill().map((_) => new Array(width).fill(filler));
+
 class LightGrid {
+  #grid;
+
+  constructor() {
+    this.#grid = makeGrid(1000, 1000, false);
+  }
+
   execute() {}
+  
   countLitLights() {
-    return 0;
+    const lightsInRow = (row) => row;
+    const isLit = (light) => light;
+
+    return this.#grid.flatMap(lightsInRow).filter(isLit).length;
   }
 }
 
