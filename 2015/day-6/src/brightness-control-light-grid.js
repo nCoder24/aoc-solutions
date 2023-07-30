eff1a642b7ada8b303531202af289474035db5ee
Utsab Bera <nCoder24@users.noreply.github.com>
2023-07-30 17:31:05 +0530

    feat: add brightness controllable light grid

---
+++ 2015/day-6/src/brightness-control-light-grid.js
@@ -0,0 +1,50 @@
+class BrightnessControlLightGrid {
+  #grid;
+
+  constructor() {
+    this.#grid = this.#makeGrid(1000, 1000);
+  }
+
+  #makeGrid(height, width) {
+    return new Array(height).fill().map(() => new Array(width).fill(0));
+  }
+
+  #getLights(start, end) {
+    const lights = [];
+
+    for (let x = start.x; x <= end.x; x++) {
+      for (let y = start.y; y <= end.y; y++) {
+        lights.push({ x, y });
+      }
+    }
+
+    return lights;
+  }
+
+  increaseBrightness(start, end) {
+    this.#getLights(start, end).forEach(({ x, y }) => {
+      this.#grid[x][y]++;
+    });
+  }
+
+  increaseBrightnessByTwo(start, end) {
+    this.#getLights(start, end).forEach(({ x, y }) => {
+      this.#grid[x][y] += 2;
+    });
+  }
+
+  decreaseBrightness(start, end) {
+    this.#getLights(start, end).forEach(({ x, y }) => {
+      this.#grid[x][y]--;
+    });
+  }
+
+  countBrightness() {
+    const lightsInRow = (row) => row;
+    const addBrightness = (total, brightness) => total + brightness;
+
+    return this.#grid.flatMap(lightsInRow).reduce(addBrightness);
+  }
+}
+
+module.exports = BrightnessControlLightGrid;
---
+++ 2015/day-6/test/brightness-control-light-grid-test.js
@@ -0,0 +1,46 @@
+const assert = require("assert");
+const { describe, it, beforeEach } = require("node:test");
+const BrightnessControlLightGrid = require("../src/brightness-control-light-grid");
+
+describe("LightGrid", () => {
+  let lightGrid;
+
+  beforeEach(() => {
+    lightGrid = new BrightnessControlLightGrid();
+  });
+
+  describe("increaseBrightness", () => {
+    it("should turn on all lights from start to end", () => {
+      lightGrid.increaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      assert.strictEqual(lightGrid.countBrightness(), 9);
+    });
+
+    it("should increase brightness of the lights from start to end", () => {
+      lightGrid.increaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      lightGrid.increaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      assert.strictEqual(lightGrid.countBrightness(), 18);
+    });
+  });
+
+  describe("decreaseBrightness", () => {
+    it("should turn off all lights from start to end if brightness is 1", () => {
+      lightGrid.increaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      lightGrid.decreaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      assert.strictEqual(lightGrid.countBrightness(), 0);
+    });
+
+    it("should decrease brightness of the lights from start to end", () => {
+      lightGrid.increaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      lightGrid.increaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      lightGrid.decreaseBrightness({ x: 0, y: 0 }, { x: 2, y: 2 });
+      assert.strictEqual(lightGrid.countBrightness(), 9);
+    });
+  });
+
+  describe("increaseBrightnessByTwo", () => {
+    it("should increase brightness all lights from start to end by two", () => {
+      lightGrid.increaseBrightnessByTwo({ x: 0, y: 0 }, { x: 2, y: 2 });
+      assert.strictEqual(lightGrid.countBrightness(), 18);
+    });
+  });
+});
